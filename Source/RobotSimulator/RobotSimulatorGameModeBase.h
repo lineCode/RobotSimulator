6894d5f2b2df42ae3020111e7328d97422b02c4a
MarceloJA <117128879+MarceloJATR@users.noreply.github.com>
2022-11-01 01:33:20 -0300

    Initial commit

---
+++ Source/RobotSimulator/RobotSimulatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RobotSimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTSIMULATOR_API ARobotSimulatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
